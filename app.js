8008eab192116d79e975c18d8ef5d5cfbdd9b2ac
Andrei Dzebysh <presnia@yahoo.com>
2020-11-08 14:16:59 +0300

    Tabs v.1 is created

---
+++ app.js
@@ -0,0 +1,46 @@
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn');
+const tabsItems = document.querySelectorAll('.tabs__item');
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener('click', () => {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
+
+
+
+        if (!currentBtn.classList.contains('active')) {
+            tabsBtn.forEach((item) => {
+            item.classList.remove('active');
+        });
+
+            tabsItems.forEach((item) => {
+            item.classList.remove('active');
+        });
+
+        currentBtn.classList.add('active');
+        currentTab.classList.add('active');
+        }
+        
+  });
+}
+
+document.querySelector('.tabs__nav-btn').click();
+
+
+
+
+/* const getElements = element.forEach((item) => {
+            item.classList.remove('active');
+        });
+
+        if (!currentBtn.classList.contains('active')) {
+            getElements(tabsBtn);
+
+            getElements(tabsItems);
+
+        currentBtn.classList.add('active');
+        currentTab.classList.add('active');
+        } */
